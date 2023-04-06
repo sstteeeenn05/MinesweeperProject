e1e3701281bd29fe1b21afc2a820bde80c712975
sstteeeenn05 <sstteeeenn05@gmail.com>
2023-04-06 19:08:33 +0800

    HomeWindow init & add some header file

--- MinesweeperProject/HomeWindow.cpp
+++ MinesweeperProject/HomeWindow.cpp
@@ -1,5 +1,13 @@
 #include "HomeWindow.h"
 
 HomeWindow::HomeWindow() {
-	window = new Fl_Window(400, 300, "Home");
+	window = new Fl_Window(WINDOW_WIDTH, WINDOW_HEIGHT, "Minesweeper");
+	window->begin();
+	logo = new Fl_Button(LOGO_X, LOGO_Y, LOGO_WIDTH, LOGO_HEIGHT);
+	logo->image((new Fl_PNG_Image("logo.png"))->copy(LOGO_WIDTH, LOGO_HEIGHT));
+	for (auto buttonText = buttonTextList.begin(); buttonText != buttonTextList.end(); buttonText++) {
+		static int buttonCount = 0;
+		buttonList.push_back(new Fl_Button(BUTTON_X + (BUTTON_MARGIN + BUTTON_WIDTH) * (buttonCount++), BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT, *buttonText));
+	}
+	window->end();
 }
--- MinesweeperProject/HomeWindow.h
+++ MinesweeperProject/HomeWindow.h
@@ -1,10 +1,34 @@
 #pragma once
+#include <map>
+#include <vector>
 #include "FL/Fl.h"
+#include "FL/Fl_Button.h"
+#include "FL/Fl_PNG_Image.H"
 #include "FL/Fl_Window.h"
+#define WINDOW_WIDTH 410
+#define WINDOW_HEIGHT 210
+#define WINDOW_PADDING 10
+#define BUTTON_X 10
+#define BUTTON_Y 160
+#define LOGO_X 10
+#define LOGO_Y 10
+#define LOGO_WIDTH 390
+#define LOGO_HEIGHT 140
+#define BUTTON_MARGIN 10
+#define BUTTON_WIDTH 90
+#define BUTTON_HEIGHT 40
+
+struct Button {
+	const char* text;
+	Fl_Button* component;
+};
 
 class HomeWindow{
 public:
+	Fl_Button* logo;
 	Fl_Window* window;
+	std::vector<const char*> buttonTextList = {"New Game","Load","Config","Exit"};
+	std::vector<Fl_Button*> buttonList;
 	HomeWindow();
 
 };
