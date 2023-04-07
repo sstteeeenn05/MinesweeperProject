0ec5dcb557ea4bb35e1be6e03c180bc6f7971829
ASOIR <cn.921221@gmail.com>
2023-04-08 00:34:17 +0800

    Complete Board class

--- MinesweeperProject/Board.cpp
+++ MinesweeperProject/Board.cpp
@@ -1,11 +1,10 @@
 #include "Board.h"
 #include <algorithm>
 #include <random>
-#include <iostream>
 #include <exception>
 
 void Board::initializeBoards() {
-    board = std::vector<std::vector<char>>
+    boardArgs.board = std::vector<std::vector<char>>
             (row, std::vector<char>(column, '#'));
 
     answerBoard = std::vector<std::vector<char>>
@@ -17,14 +16,15 @@ void Board::calculateAnswerByInput(std::ifstream &inputFile) {
         for (int j = 0; j < column; j++) {
             if (inputFile.get() == 'X') {
                 answerBoard[i][j] = 'X';
+                bombCount++;
                 plusOneAroundTheMine(j, i);
             }
         }
         // check column count is valid
-        if(inputFile.get()!='\n' && !inputFile.eof()) throw std::exception();
+        if (inputFile.get() != '\n' && !inputFile.eof()) throw std::exception("File format error.");
 
         // check row count is valid
-        if(inputFile.eof() && i!=row-1) throw std::exception();
+        if (inputFile.eof() && i != row - 1) throw std::exception("File format error.");
     }
 }
 
@@ -36,6 +36,7 @@ void Board::randomTheBoard(double randomRate) {
         for (int j = 0; j < column; j++) {
             if (generator() % 100 + 1 < mineRange) {
                 answerBoard[i][j] = 'X';
+                bombCount++;
                 plusOneAroundTheMine(j, i);
             }
         }
@@ -52,8 +53,41 @@ void Board::plusOneAroundTheMine(int x, int y) {
     }
 }
 
+void Board::countBlank() {
+    int tempBlankCount = 0;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            if ((boardArgs.board[i][j] == '#' ||
+                 boardArgs.board[i][j] == 'f' ||
+                 boardArgs.board[i][j] == '?') &&
+                answerBoard[i][j] != 'X')
+                tempBlankCount++;
+        }
+    }
+
+    remainBlankCount = tempBlankCount;
+    openBlankCount = row * column - bombCount - remainBlankCount;
+}
+
 bool Board::isCoordinateValid(int x, int y) const {
     return (x >= 0 && x < column && y >= 0 && y < row);
+}
+
+void Board::revealGrid(int x, int y) {
+    for (int i = y - 1; i <= y + 1; i++) {
+        for (int j = x - 1; j <= x + 1; j++) {
+            if (isCoordinateValid(j, i)) {
+                if (answerBoard[i][j] == '0' &&
+                    (boardArgs.board[i][j] == '#'
+                     || boardArgs.board[i][j] == '?')) {
+                    boardArgs.board[i][j] = answerBoard[i][j];
+                    revealGrid(j, i);
+                }
+                else if (answerBoard[i][j] != 'X' && boardArgs.board[i][j] != 'f')
+                    boardArgs.board[i][j] = answerBoard[i][j];
+            }
+        }
+    }
 }
 
 Board::Board() = default;
@@ -63,41 +97,93 @@ Board::Board(std::ifstream &inputFile) {
     inputFile.get();
     if (row <= 0 && column <= 0) {
         inputFile.close();
-        throw std::exception();
+        throw std::exception("Row or column out of range.");
     }
+
     initializeBoards();
     calculateAnswerByInput(inputFile);
     inputFile.close();
+    countBlank();
 }
 
 Board::Board(int inputRow, int inputColumn, double randomRate) : row(inputRow), column(inputColumn) {
-    if (row <= 0 && column <= 0) throw std::exception();
-    if(randomRate<0) throw std::exception();
+    if (row <= 0 && column <= 0) throw std::exception("Row or column out of range.");
+    if (randomRate < 0) throw std::exception("Random rate out of range.");
+
     initializeBoards();
     randomTheBoard(randomRate);
+    countBlank();
 
 }
 
 Board::Board(int inputRow, int inputColumn, int mineCount) : row(inputRow), column(inputColumn) {
-    if (row <= 0 && column <= 0) throw std::exception();
-    if (mineCount > row * column) throw std::exception();
-    initializeBoards();
+    if (row <= 0 && column <= 0) throw std::exception("Row or column out of range.");
+    if (mineCount > row * column) throw std::exception("Mine count out of range.");
+    bombCount = mineCount;
 
+    initializeBoards();
     std::random_device dev;
     std::mt19937 generator(dev());
 
-    std::vector<char> tempBoard(row*column,'0');
-    std::fill_n(tempBoard.begin(),mineCount,'X');
-    std::shuffle(tempBoard.begin(),tempBoard.end(),generator);
+    std::vector<char> tempBoard(row * column, '0');
+    std::fill_n(tempBoard.begin(), mineCount, 'X');
+    std::shuffle(tempBoard.begin(), tempBoard.end(), generator);
 
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < column; j++) {
-            if (tempBoard[i*row+j]=='X') {
+            if (tempBoard[i * row + j] == 'X') {
                 answerBoard[i][j] = 'X';
                 plusOneAroundTheMine(j, i);
             }
         }
     }
+
+    countBlank();
+}
+
+void Board::leftClick(int x, int y) {
+    if (!isCoordinateValid(x, y)) throw std::exception("X or Y out of range.");
+
+    if (boardArgs.board[y][x] != 'f') boardArgs.board[y][x] = answerBoard[y][x];
+    else return;
+
+    openBlankCount++;
+    remainBlankCount--;
+
+    if (answerBoard[y][x] == 'X') {
+        boardArgs.gameStatus = LOSE;
+        return;
+    }
+
+    for (int i = y - 1; i <= y + 1; i++) {
+        for (int j = x - 1; j <= x + 1; j++) {
+            if (isCoordinateValid(j, i)) {
+                if (answerBoard[i][j] == '0' &&
+                    (boardArgs.board[i][j] == '#'
+                     || boardArgs.board[i][j] == '?')) {
+                    revealGrid(j, i);
+                }
+            }
+        }
+    }
+
+    countBlank();
+    if (remainBlankCount == 0)boardArgs.gameStatus = WIN;
+}
+
+void Board::rightClick(int x, int y) {
+    if (!isCoordinateValid(x, y)) throw std::exception("X or Y out of range.");
+    constexpr char symbol[3] = {'#', 'f', '?'};
+
+    for (int i = 0; i < 3; i++) {
+        if (boardArgs.board[y][x] == symbol[i]) {
+            boardArgs.board[y][x] = symbol[((i == 2) ? 0 : (i + 1))];
+            if (symbol[i] == 'f') flagCount--;
+            else flagCount++;
+            return;
+        }
+    }
+
 }
 
 int Board::getRow() const {
@@ -108,10 +194,26 @@ int Board::getColumn() const {
     return column;
 }
 
-std::vector<std::vector<char>> Board::getAnswer() const {
+int Board::getBombCount() const {
+    return bombCount;
+}
+
+int Board::getFlagCount() const {
+    return flagCount;
+}
+
+int Board::getRemainBlank() const {
+    return remainBlankCount;
+}
+
+int Board::getOpenBlank() const {
+    return openBlankCount;
+}
+
+const std::vector<std::vector<char>> &Board::getAnswer() const {
     return answerBoard;
 }
 
-std::vector<std::vector<char>> Board::getCurrentBoard() const {
-    return board;
+const BoardArgs &Board::getCurrentBoard() const {
+    return boardArgs;
 }
--- MinesweeperProject/Board.h
+++ MinesweeperProject/Board.h
@@ -3,13 +3,25 @@
 #include <vector>
 #include <fstream>
 
+constexpr int WIN = 2;
+constexpr int LOSE = 1;
+constexpr int CONTINUE = 0;
+
+struct BoardArgs {
+    std::vector<std::vector<char>> board;
+    int gameStatus = CONTINUE;
+};
+
 class Board {
 private:
-    std::vector<std::vector<char>> board;
+    BoardArgs boardArgs;
     std::vector<std::vector<char>> answerBoard;
     int row = 0;
     int column = 0;
-
+    int bombCount = 0;
+    int flagCount = 0;
+    int openBlankCount = 0;
+    int remainBlankCount = 0;
 
     void initializeBoards();
 
@@ -19,12 +31,14 @@ private:
 
     void plusOneAroundTheMine(int x, int y);
 
+    void countBlank();
+
     bool isCoordinateValid(int x, int y) const;
+
+    void revealGrid(int x, int y);
 
 public:
 
-    //TODO: 1.leftClick and rightClick
-     
     Board();
 
     explicit Board(std::ifstream &inputFile);
@@ -33,12 +47,24 @@ public:
 
     Board(int inputRow, int inputColumn, int mineCount);
 
+    void leftClick(int x, int y);
+
+    void rightClick(int x, int y);
+
     int getRow() const;
 
     int getColumn() const;
 
-    std::vector<std::vector<char>> getAnswer() const;
+    int getBombCount() const;
 
-    std::vector<std::vector<char>> getCurrentBoard() const;
+    int getFlagCount() const;
+
+    int getRemainBlank() const;
+
+    int getOpenBlank() const;
+
+    const std::vector<std::vector<char>> &getAnswer() const;
+
+    const BoardArgs &getCurrentBoard() const;
 
 };
